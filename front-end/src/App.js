876c240a78c2d0063daecc2c540a56773f4f55c1
JoseR23 <joserevelo0601@hotmail.com>
2024-07-22 22:27:27 -0500

    hice la navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RecuperarPass from './components/RecuperarPass';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/recuperar-pass" element={<RecuperarPass />} />
         <Route path="*" element={<LoginPage />} /> {/* Default to LoginPage */}
         <Route path="/home" element={<HomePage />} />
+        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </Router>
   );
